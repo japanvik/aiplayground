0c6f2bbe25a4d1486e3e28c6c735eae9551312ab
Vik Kumar <vik@japanvik.net>
2020-09-02 18:20:22 +0900

    initial commit

---
+++ dataloaders/blackandwhite.py
@@ -0,0 +1,36 @@
+# Black and white dataset based on a color picture
+
+from PIL import Image
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
+
+class BlackAndWhite(ImageFolder):
+
+    def __getitem__(self, index):
+    """
+    Args:
+        index (int): Index
+
+    Returns:
+        tuple: (source target) source is BnW image, target is color
+    """
+    path, target = self.samples[index]
+    sample = self.loader(path)
+    # Transforms
+    image_transform = transforms.Compose(transforms.Resize((288,288)),
+            transforms.RandomRotation(degrees=5.0),
+            transforms.RandomCrop(256))
+    gray_transform = transforms.Compose(transforms.Grayscale(num_output_channels=3),)
+    normalize_transform = transforms.Compose([ transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) ])
+
+    # Resize, rotate and crop
+    target = image_transform(sample)
+    source = gray_transform(target)
+    #
+    target = normalize_transform(target)
+    source = normalize_transform(source)
+
+    return source, target
+
+
